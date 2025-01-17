81269f1064a9a6505a1e91926c7c923a46a41495
anislamichhane <anislamichhane0@gmail.com>
2025-01-17 20:20:12 +0545

    if/else statement

---
+++ day03/day03.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your hieght in cm? "))
+
+if height > 120:
+    print("Congratulation now you can ride a rollercoaster! ")
+
+else:
+    print("You cannot ride a rollercoaster! ")
